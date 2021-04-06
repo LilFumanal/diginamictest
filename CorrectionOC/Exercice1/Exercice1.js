8f5837951b30b13a0b728e4ce7334fedcddffcbf
LilFumanal <80684037+LilFumanal@users.noreply.github.com>
2021-04-06 11:57:17 +0200

    Exercice 1

---
+++ CorrectionOC/Exercice1/Exercice1.js
@@ -0,0 +1,8 @@
+const listeEl = document.querySelector("article > ul.important > li");
+
+const mainContent = document.getElementById("main-content");
+const important = document.getElementsByClassName("important");
+const articles = document.getElementsByTagName("article");
+
+
+console.log(listeEl.nextElementSibling);
